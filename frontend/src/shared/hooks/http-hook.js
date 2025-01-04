fc718f56ffcc70ad99520df48fba2f56e4681d13
Abdou Faye <abdoufaye2021@gmail.com>
2025-01-04 01:32:32 +0000

    update

--- frontend/src/shared/hooks/http-hook.js
+++ frontend/src/shared/hooks/http-hook.js
@@ -4,5 +4,11 @@ export const useHttpClient = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
-  const sendRequest = (url, method = "GET", body = null, headers = {}) => {};
+  const sendRequest = (url, method = "GET", body = null, headers = {}) => {
+    fetch(url, {
+      method, // equivaut a method:method
+      body,
+      headers,
+    });
+  };
 };
