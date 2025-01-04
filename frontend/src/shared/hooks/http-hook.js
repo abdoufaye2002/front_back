23bf2402bb63d2ce7a38d80511161f8cd788079c
Abdou Faye <abdoufaye2021@gmail.com>
2025-01-04 01:53:59 +0000

    update

--- frontend/src/shared/hooks/http-hook.js
+++ frontend/src/shared/hooks/http-hook.js
@@ -1,31 +1,32 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export const useHttpClient = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
-  const sendRequest = async (
-    url,
-    method = "GET",
-    body = null,
-    headers = {}
-  ) => {
-    setIsLoading(true);
-    try {
-      const response = await fetch(url, {
-        method, // equivaut a method:method
-        body,
-        headers,
-      });
-      const responseData = await response.json();
-      if (!response.ok) {
-        throw new Error(response.message);
+  const sendRequest = useCallback(
+    async (url, method = "GET", body = null, headers = {}) => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url, {
+          method, // equivaut a method:method
+          body,
+          headers,
+        });
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(response.message);
+        }
+        return responseData;
+      } catch (err) {
+        setError(error.message);
       }
-      return responseData;
-    } catch (err) {
-      setError(error.message);
-    }
-    setIsLoading(false);
+      setIsLoading(false);
+    },
+    []
+  );
+  const clearError = () => {
+    setError(null);
   };
   return { isLoading, error, sendRequest }; // equivaut a isLoading:isLoading
 };
