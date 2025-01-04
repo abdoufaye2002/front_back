36a71ad9cd9ea1cf1baf6905e6cadaf85164f26e
Abdou Faye <abdoufaye2021@gmail.com>
2025-01-04 01:28:49 +0000

    update

--- frontend/src/shared/hooks/http-hook.js
+++ frontend/src/shared/hooks/http-hook.js
@@ -1,2 +1,8 @@
-export const useHttpClient = () => {};
-//
+import { useState } from "react";
+
+export const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const sendRequest = (url, method = "GET", body = null, headers = {}) => {};
+};
